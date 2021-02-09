a69be19ab21f3126bec40d18d87a7e1672af3c07
Shalli Goel <shalligoel@users.noreply.github.com>
2021-02-09 07:13:12 +0530

    Update config.js

--- database/config.js
+++ database/config.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   port     : '3306',
   user     : 'dress4win',
-  password : 'P@root123',
+  password : 'root123',
   database : 'eCommerce'
 });
 
